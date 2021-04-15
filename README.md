ea66660577b1e2f676e7a9823754e0d196c1f7b7
mirwox <fmiranda@gmail.com>
2021-04-14 23:37:54 -0300

    envio para testes no linux

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# 211_robot_p1
+# Robótica 2020.2 P2
+
+Robótica 2020 - Prova 2 - AF
+
+# Procedimento
+
+O código deve ser submetido via **Github classroom**
+
+O Blackboard deve ser mantido aberto todo o tempo
+
+
+# Enunciado
+
+
+[Enunciado da prova](enunciado.md)
+
+[Enunciado da prova, só que no Jupyter para o preview do Github ficar bom](enunciado.ipynb)
+
+[Reunião da Prova no MS Teams - será usada só para a abertura e chat para avisos **FIQUE ATENTO*](d)
+
+**Planilha de atendimento**
+Avise aqui se tiver problemas e ligamos para você. *Não* vamos dar manutenção básica nem explicar teoria hoje.   [https://docs.google.com/spreadsheets/d/1Q8ThPMXTfbh_H2zSbVjTuhuUA1ruOJKZgrLuyw8NboY/edit?usp=sharing](https://docs.google.com/spreadsheets/d/1Q8ThPMXTfbh_H2zSbVjTuhuUA1ruOJKZgrLuyw8NboY/edit?usp=sharing)
+
+
+**Para entregar, faça commits e pushes regulares**
+
+**Integridade Intelectual**
+
+Se você tiver alguma evidência de fraude cometida nesta prova, [use este serviço de e-mail anônimo](https://www.guerrillamail.com/pt/compose)  para informar ao professor.  Ou [este formulário](https://forms.gle/JPhqjPmuKAHxmvwZ9)
+
---
+++ p1_211/scripts/Q3.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function, division
+
+
+# Para rodar, recomendamos que faça:
+# 
+#    roslaunch my_simulation pista_u.launch
+#
+# Depois o controlador do braço:
+#
+#    roslaunch mybot_description mybot_control2.launch 	
+import rospy 
+
+import numpy as np
+
+import cv2
+
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+import math
+
+
+ranges = None
+minv = 0
+maxv = 10
+
+bridge = CvBridge()
+
+
+def scaneou(dado):
+    global ranges
+    global minv
+    global maxv
+    print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+    print("Leituras:")
+    ranges = np.array(dado.ranges).round(decimals=2)
+    minv = dado.range_min 
+    maxv = dado.range_max
+ 
+# A função a seguir é chamada sempre que chega um novo frame
+def roda_todo_frame(imagem):
+    print("frame")
+    try:
+        cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+        cv2.imshow("Camera", cv_image)
+        cv2.waitKey(1)
+    except CvBridgeError as e:
+        print('ex', e)
+
+if __name__=="__main__":
+
+    rospy.init_node("q3")
+
+    topico_imagem = "/camera/image/compressed"
+    velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+    recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+    recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
