5ad973da2b328ea3c54226ea3fb031d2a48d2139
DiegoSoler <dpsoler09@gmail.com>
2021-04-28 00:55:48 -0300

    Update q2.py
    Q2 resolvido. Tentei aproveitar o maximo a Q1

--- q2/q2.py
+++ q2/q2.py
@@ -16,26 +16,87 @@ print("Diretório de trabalho: ", os.getcwd())
 # Arquivos necessários
 video = "bandeiras_movie.mp4"
 
+def encontrar_contornos(mask):
+    """Não mude ou renomeie esta função
+        deve receber uma imagem preta e branca os contornos encontrados
+    """
+    # RETR_EXTERNAL: Apenas Contornos Externos
+    contornos, arvore = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    return contornos, arvore
+
+def encontrar_caixa_do_contorno(contornos):
+    """
+    Encontra a "bounding box" de cada contorno.
+    """
+    box = []
+    for contorno in contornos:
+        (x,y,w,h) = cv2.boundingRect(contorno)
+        box.append((x,y,w,h))
+
+    return box
+    
+def escrever_textos(frame,box, color, text):
+    """
+    Escrever os pontos P1 e P2, a bandeira e desenhar um retangulo envolta da bandeira.
+    """
+    (x,y,w,h) = box
+    
+    # Retangulo
+    cv2.rectangle(frame, (x,y), (x+w,y+h), color, 3)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # Flag
+    cv2.putText(frame, text, (x,y+5*h//4), font, 0.75, color, 2, cv2.LINE_AA)
+    # P1
+    cv2.putText(frame, 'P1 (%s,%s)'%(x,y), (x,y-5), font, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    # P2
+    cv2.putText(frame, 'P2 (%s,%s)'%(x+w,y+h), (x+w+5,y+h), font, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    
+
+    return frame, ((x,y),(x+w,y+h))
+
+
+######
 corner_jp = ((10,10),(100,100))
 corner_pl = ((5,5),(200,200))
-
 
 # Responda dentro desta função. 
 # Pode criar e chamar novas funções o quanto quiser
 def encontra_japao_polonia_devolve_corners(bgr):
     frame = bgr.copy()
+
+    ## Converter Para Cinza
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ## Filtrar Cor
+    output = cv2.inRange(gray,200,255)
+    # Para remover FP:
+    output = cv2.morphologyEx(output, cv2.MORPH_CLOSE, np.ones((10,10),np.uint8))
+
+    ## Encontrar Contornos
+    contornos, _ = encontrar_contornos(output)
+
+    ## Ordenando eh Possivel Separar Apenas Japao e Polonia
+    contornos = sorted(contornos,key=cv2.contourArea)[5:7]
+
+    ## Encontrar Bounding Box
+    box = encontrar_caixa_do_contorno(contornos)
+
+    ## Japao
+    frame, corner_jp = escrever_textos(frame, box[1], (255,0,0), 'Japao')
+
+    ## Polonia
+    (x,y,w,h) = box[0]
+    # Dobrar a Altura do Retangulo Para Incluir o Retangulo Vermelho
+    frame, corner_pl = escrever_textos(frame, (x,y,w,h*2), (0,255,0), 'Polonia')
+
     return frame, corner_jp, corner_pl
-
-
-
-
-
 
 if __name__ == "__main__":
 
     # Inicializa a aquisição da webcam
     cap = cv2.VideoCapture(video)
-
 
     print("Se a janela com a imagem não aparecer em primeiro plano dê Alt-Tab")
 
