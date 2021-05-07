e233f9e128fa7e2aada14481caa59e38af0992fc
mirwox <fmiranda@gmail.com>
2021-05-07 07:16:59 -0700

    Q3 utils adicionados

--- p1_211/scripts/Q3.py
+++ p1_211/scripts/Q3.py
@@ -30,59 +30,8 @@ from geometry_msgs.msg import Pose
 from scipy.spatial.transform import Rotation as R
 
 import math
+import Q3_utils as q3utils
 
-
-## Código vindo de https://github.com/Insper/robot21.1/blob/main/aula03/centro_do_amarelo.py
-
-low = np.array([22, 50, 50],dtype=np.uint8)
-high = np.array([36, 255, 255],dtype=np.uint8)
-
-def filter_color(bgr, low, high):
-    """ REturns a mask within the range"""
-    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, low, high)
-    return mask     
-
-# Função centro de massa baseada na aula 02  https://github.com/Insper/robot202/blob/master/aula02/aula02_Exemplos_Adicionais.ipynb
-# Esta função calcula centro de massa de máscara binária 0-255 também, não só de contorno
-def center_of_mass(mask):
-    """ Retorna uma tupla (cx, cy) que desenha o centro do contorno"""
-    M = cv2.moments(mask)
-    # Usando a expressão do centróide definida em: https://en.wikipedia.org/wiki/Image_moment
-    m00 = max(M["m00"],1) # para evitar dar erro quando não há contornos
-    cX = int(M["m10"] / m00)
-    cY = int(M["m01"] / m00)
-    return [int(cX), int(cY)]
-
-def crosshair(img, point, size, color):
-    """ Desenha um crosshair centrado no point.
-        point deve ser uma tupla (x,y)
-        color é uma tupla R,G,B uint8
-    """
-    x,y = point
-    cv2.line(img,(x - size,y),(x + size,y),color,5)
-    cv2.line(img,(x,y - size),(x, y + size),color,5)
-
-def center_of_mass_region(mask, x1, y1, x2, y2):
-    # Para fins de desenho
-    mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    clipped = mask[y1:y2, x1:x2]
-    c = center_of_mass(clipped)
-    c[0]+=x1
-    c[1]+=y1
-    crosshair(mask_bgr, c, 10, (0,0,255))
-    cv2.rectangle(mask_bgr, (x1, y1), (x2, y2), (255,0,0),2,cv2.LINE_AA)
-    centro = (int(c[0]), int(c[1]))
-    return mask_bgr, centro
-
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-def texto(img, a, p):
-    """Escreve na img RGB dada a string a na posição definida pela tupla p"""
-    cv2.putText(img, str(a), p, font,1,(0,50,100),2,cv2.LINE_AA)
-    
-
-## Fim do código vindo de https://github.com/Insper/robot21.1/blob/main/aula03/centro_do_amarelo.py
 
 
 ranges = None
@@ -125,61 +74,6 @@ def scaneou(dado):
     distancia = ranges[0]
 
 
-## Código vindo do notebook
-# 
-#  https://github.com/Insper/robot21.1/blob/main/aula03/aula03_RegressaoPixelsAmarelos.ipynb
-# 
-# ##
-
-import statsmodels.api as sm
-
-
-def ajuste_linear_x_fy(mask):
-    """Recebe uma imagem já limiarizada e faz um ajuste linear
-        retorna coeficientes linear e angular da reta
-        e equação é da forma
-        x = coef_angular*y + coef_linear
-    """ 
-    pontos = np.where(mask==255)
-    ximg = pontos[1]
-    yimg = pontos[0]
-    yimg_c = sm.add_constant(yimg)
-    model = sm.OLS(ximg,yimg_c)
-    results = model.fit()
-    coef_angular = results.params[1] # Pegamos o beta 1
-    coef_linear =  results.params[0] # Pegamso o beta 0
-    return coef_angular, coef_linear, pontos # Pontos foi adicionado para performance, como mencionado no notebook
-
-
-def ajuste_linear_grafico_x_fy(mask_in): 
-    """Faz um ajuste linear e devolve uma imagem rgb com aquele ajuste desenhado sobre uma imagem
-       Trabalhando com x em funcão de y
-    """
-
-    # vamos criar uma imagem com 50% do tamanho para acelerar a regressao 
-    # isso nao afeta muito o angulo
-
-    scale_percent = 50 # percent of original size
-    width = int(mask_in.shape[1] * scale_percent / 100)
-    height = int(mask_in.shape[0] * scale_percent / 100)
-    dim = (width, height)
-
-    mask = cv2.resize(mask_in, dim)
-
-    coef_angular, coef_linear, pontos  = ajuste_linear_x_fy(mask)
-    print("x = {:3f}*y + {:3f}".format(coef_angular, coef_linear))
-    ximg = pontos[1]
-    yimg = pontos[0]
-    y_bounds = np.array([min(yimg), max(yimg)])
-    x_bounds = coef_angular*y_bounds + coef_linear
-    print("x bounds", x_bounds)
-    print("y bounds", y_bounds)
-    x_int = x_bounds.astype(dtype=np.int64)
-    y_int = y_bounds.astype(dtype=np.int64)
-    mask_bgr =  cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-    cv2.line(mask_bgr, (x_int[0], y_int[0]), (x_int[1], y_int[1]), color=(0,0,255), thickness=11);    
-    return mask_bgr, coef_angular, coef_linear
-
 
 ## Variáveis novas criadas pelo gabarito
 
@@ -188,6 +82,9 @@ frame = 0
 skip = 3
 m = 0
 angle_yellow = 0 # angulo com a vertical
+
+low = q3utils.low
+high = q3utils.high
 
 ## 
 
@@ -204,15 +101,16 @@ def roda_todo_frame(imagem):
         copia = cv_image.copy() # se precisar usar no while
 
         if frame%skip==0: # contamos a cada skip frames
-            mask = filter_color(copia, low, high)                
-            img, centro_yellow  =  center_of_mass_region(mask, 0, 300, mask.shape[1], mask.shape[0])  
+            mask = q3utils.filter_color(copia, low, high)                
+            img, centro_yellow  =  q3utils.center_of_mass_region(mask, 0, 300, mask.shape[1], mask.shape[0])  
 
-            saida_bgr, m, h = ajuste_linear_grafico_x_fy(mask)
+            saida_bgr, m, h = q3utils.ajuste_linear_grafico_x_fy(mask)
 
             ang = math.atan(m)
             ang_deg = math.degrees(ang)
 
-            texto(saida_bgr, f"Angulo: {ang_deg}", (100,50))
+            q3utils.texto(saida_bgr, f"Angulo graus: {ang_deg}", (15,50))
+            q3utils.texto(saida_bgr, f"Angulo rad: {ang}", (15,90))
 
             cv2.imshow("centro", img)
             cv2.imshow("angulo", saida_bgr)
