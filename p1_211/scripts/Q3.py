0a6d71d1dc28d0c3ffb60ea669b040b86604a202
mirwox <fmiranda@gmail.com>
2021-05-11 10:22:02 -0700

    Q3_WIP

--- p1_211/scripts/Q3.py
+++ p1_211/scripts/Q3.py
@@ -86,6 +86,9 @@ angle_yellow = 0 # angulo com a vertical
 low = q3utils.low
 high = q3utils.high
 
+centro_caixa = (320, 240)
+area_caixa = 0
+
 ## 
 
 # A função a seguir é chamada sempre que chega um novo frame
@@ -93,6 +96,11 @@ def roda_todo_frame(imagem):
     global centro_yellow
     global m
     global angle_yellow
+    global centro_caixa
+    global area_caixa
+
+    ### 
+    ## Vamos fazer o gabarito para a caixa azul, que era mais distante 
 
     try:
         cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
@@ -109,6 +117,8 @@ def roda_todo_frame(imagem):
             ang = math.atan(m)
             ang_deg = math.degrees(ang)
 
+            angle_yellow = ang_deg
+
             q3utils.texto(saida_bgr, f"Angulo graus: {ang_deg}", (15,50))
             q3utils.texto(saida_bgr, f"Angulo rad: {ang}", (15,90))
 
@@ -116,9 +126,21 @@ def roda_todo_frame(imagem):
             cv2.imshow("angulo", saida_bgr)
 
 
+            ## Achando o maior objeto azul 
+            media, centro_frame, area = q3utils.identifica_cor(copia)
+            
+            area_caixa = area
+            centro_caixa = media
+
+
+
         cv2.waitKey(1)
     except CvBridgeError as e:
         print('ex', e)
+
+
+
+
 
 if __name__=="__main__":
 
@@ -126,10 +148,80 @@ if __name__=="__main__":
 
     topico_imagem = "/camera/image/compressed"
     velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+    cmd_vel = velocidade_saida
 
     recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
     pose_sub = rospy.Subscriber('/odom', Odometry , mypose)
     recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
 
+    zero = Twist(Vector3(0,0,0), Vector3(0,0,0))         
+
+
+    x = 0
+    tol_centro = 10 # tolerancia de fuga do centro
+    tol_ang = 15 # tolerancia do angulo
+    area_ideal_caixa = 10000
+
+    c_img = 320 # Centro da imagem
+
+    v_slow = 0.2
+    v_rapido = 0.5
+    w_slow = 0.1 
+    w_rapido = 0.6
+
+    
+    INICIAL= -1
+    AVANCA = 0
+    AVANCA_RAPIDO = 1
+    ALINHA = 2
+    AVANCA_PROXIMO = 3
+    TERMINOU = 4
+
+    state = INICIAL
+
+    def inicial():
+        # Ainda sem uma ação específica
+        pass
+
+    def avanca():
+        vel = Twist(Vector3(v_slow,0,0), Vector3(0,0,0)) 
+        cmd_vel.publish(vel) 
+
+    def avanca_rapido():
+        vel = Twist(Vector3(v_slow,0,0), Vector3(0,0,0))         
+        cmd_vel.publish(v_rapido)
+
+    def alinha():
+        delta_x = c_img - centro_centro_yellow[x]
+        max_delta = 150.0
+        w = w_min + (delta_x/max_delta)*w_rapido
+        vel = Twist(Vector3(v_slow,0,0), Vector3(0,0,w)) 
+        cmd_vel.publish(vel)        
+
+    def avanca_proximo():
+        pass
+
+    def terminou():
+        zero = Twist(Vector3(0,0,0), Vector3(0,0,0))         
+        cmd_vel.publish(zero)
+
+    def dispatch():
+        "Logica de determinar o proximo estado"
+        if area_caixa >= area_ideal_caixa:
+            state = TERMINOU
+        elif c_img - tol_centro < centro_yellow[x] < c_img + tol_centro:
+            state = AVANCA
+
+            if   - tol_ang< angle_yellow  < tol_ang:  # para angulos centrados na vertical, regressao de x = f(y) como está feito
+                state = AVANCA_RAPIDO
+        else:
+            state = ALINHA
+        print("centro_yellow {} area caixa {:.2f} angle_yellow {:.3f}".format(centro_yellow, area_caixa, angle_yellow))
+    acoes = {INICIAL:inicial, AVANCA: avanca, AVANCA_RAPIDO: avanca_rapido, ALINHA: alinha, AVANCA_PROXIMO: avanca_proximo , TERMINOU: terminou}
+
+
     while not rospy.is_shutdown():
-        rospy.sleep(0.1)
+        print("Estado: ", state)
+        acoes[state]()  # executa a funcão que está no dicionário
+        dispatch()    
+        rospy.sleep(0.05)
--- p1_211/scripts/Q3_utils.py
+++ p1_211/scripts/Q3_utils.py
@@ -101,6 +101,11 @@ def ajuste_linear_x_fy(mask):
     pontos = np.where(mask==255)
     ximg = pontos[1]
     yimg = pontos[0]
+
+    ## Caso adicionado para evitar resultados invalidos
+    if len(ximg) < 10: 
+        return 0,0, [[0],[0]]
+
     yimg_c = sm.add_constant(yimg)
     model = sm.OLS(ximg,yimg_c)
     results = model.fit()
@@ -109,7 +114,7 @@ def ajuste_linear_x_fy(mask):
     return coef_angular, coef_linear, pontos # Pontos foi adicionado para performance, como mencionado no notebook
 
 
-def ajuste_linear_grafico_x_fy(mask_in): 
+def ajuste_linear_grafico_x_fy(mask_in, print_eq = False): 
     """Faz um ajuste linear e devolve uma imagem rgb com aquele ajuste desenhado sobre uma imagem
        Trabalhando com x em funcão de y
     """
@@ -125,18 +130,104 @@ def ajuste_linear_grafico_x_fy(mask_in):
     mask = cv2.resize(mask_in, dim)
 
     coef_angular, coef_linear, pontos  = ajuste_linear_x_fy(mask)
-    print("x = {:3f}*y + {:3f}".format(coef_angular, coef_linear))
+    if print_eq: 
+        print("x = {:3f}*y + {:3f}".format(coef_angular, coef_linear))
     ximg = pontos[1]
     yimg = pontos[0]
     y_bounds = np.array([min(yimg), max(yimg)])
     x_bounds = coef_angular*y_bounds + coef_linear
-    print("x bounds", x_bounds)
-    print("y bounds", y_bounds)
+    # print("x bounds", x_bounds)
+    # print("y bounds", y_bounds)
     x_int = x_bounds.astype(dtype=np.int64)
     y_int = y_bounds.astype(dtype=np.int64)
-    mask_bgr =  cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    mask_bgr =  cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)    
     cv2.line(mask_bgr, (x_int[0], y_int[0]), (x_int[1], y_int[1]), color=(0,0,255), thickness=11);    
     return mask_bgr, coef_angular, coef_linear
 
 
 ## Fim do código vindo do notebook
+
+
+##  Código vindo da funcão cormodule.py da APS 4 
+
+import rospy
+import numpy as np
+import tf
+import math
+import cv2
+import time
+from geometry_msgs.msg import Twist, Vector3, Pose
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+import smach
+import smach_ros
+
+
+def identifica_cor(frame):
+    '''
+    Segmenta o maior objeto cuja cor é parecida com cor_h (HUE da cor, no espaço HSV).
+    '''
+
+    # No OpenCV, o canal H vai de 0 até 179, logo cores similares ao 
+    # vermelho puro (H=0) estão entre H=-8 e H=8. 
+    # Precisamos dividir o inRange em duas partes para fazer a detecção 
+    # do vermelho:
+    # frame = cv2.flip(frame, -1) # flip 0: eixo x, 1: eixo y, -1: 2 eixos
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cor_menor = np.array([100, 50, 50]) # Adaptado para azul
+    cor_maior = np.array([120, 255, 255])
+    segmentado_cor = cv2.inRange(frame_hsv, cor_menor, cor_maior)
+
+    # Note que a notacão do numpy encara as imagens como matriz, portanto o enderecamento é
+    # linha, coluna ou (y,x)
+    # Por isso na hora de montar a tupla com o centro precisamos inverter, porque 
+    centro = (frame.shape[1]//2, frame.shape[0]//2)
+
+
+    def cross(img_rgb, point, color, width,length):
+        cv2.line(img_rgb, (int( point[0] - length/2 ), point[1] ),  (int( point[0] + length/2 ), point[1]), color ,width, length)
+        cv2.line(img_rgb, (point[0], int(point[1] - length/2) ), (point[0], int( point[1] + length/2 ) ),color ,width, length) 
+
+
+
+    # A operação MORPH_CLOSE fecha todos os buracos na máscara menores 
+    # que um quadrado 7x7. É muito útil para juntar vários 
+    # pequenos contornos muito próximos em um só.
+    segmentado_cor = cv2.morphologyEx(segmentado_cor,cv2.MORPH_CLOSE,np.ones((7, 7)))
+
+    # Encontramos os contornos na máscara e selecionamos o de maior área
+    #contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)	
+    contornos, arvore = cv2.findContours(segmentado_cor.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+
+    maior_contorno = None
+    maior_contorno_area = 0
+
+    for cnt in contornos:
+        area = cv2.contourArea(cnt)
+        if area > maior_contorno_area:
+            maior_contorno = cnt
+            maior_contorno_area = area
+
+    # Encontramos o centro do contorno fazendo a média de todos seus pontos.
+    if not maior_contorno is None :
+        cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
+        maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
+        media = maior_contorno.mean(axis=0)
+        media = media.astype(np.int32)
+        cv2.circle(frame, (media[0], media[1]), 5, [0, 255, 0])
+        cross(frame, centro, [255,0,0], 1, 17)
+    else:
+        media = (0, 0)
+
+    # Representa a area e o centro do maior contorno no frame
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    cv2.putText(frame,"{:d} {:d}".format(*media),(20,100), 1, 4,(255,255,255),2,cv2.LINE_AA)
+    cv2.putText(frame,"{:0.1f}".format(maior_contorno_area),(20,50), 1, 4,(255,255,255),2,cv2.LINE_AA)
+
+   # cv2.imshow('video', frame)
+    cv2.imshow('seg', frame)
+
+    return media, centro, maior_contorno_area
+
